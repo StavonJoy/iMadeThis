8819582ee2b49e1d8811e822b2b49b54b08acda4
Stavon Ewart <stavon@Stavons-MacBook-Air.local>
2020-10-12 14:23:05 -0400

    Adds crafts index page

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
+  path('crafts/', views.crafts_index, name='index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
 def home(request):
   return HttpResponse('<h1>Home</h1>')
 
 def about(request):
   return render(request, 'about.html')
 
+def crafts_index(request):
+  return render(request, 'crafts/index.html', { 'crafts': crafts })
+
+class Craft: 
+  def __init__(self, name, type, hours, description):
+    self.name = name
+    self.type = type
+    self.hours = hours
+    self.description = description
+
+crafts = [
+  Craft('Knit Hat', 'knit', '2', 'A warm and cozy hat, made with chunky yarn and love.'),
+]
