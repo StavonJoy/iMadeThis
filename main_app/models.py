ea2ce42af46a143ccdf12533c2147e61abc7e684
Stavon Ewart <stavon@Stavons-MacBook-Air.local>
2020-10-13 09:31:40 -0400

    creates models and admin func

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Craft(models.Model): 
+  name = models.CharField(max_length=100)
+  type = models.CharField(max_length=100)
+  hours = models.IntegerField()
+  description = models.CharField(max_length=300)
+
+  def __str__(self):
+    return self.name
+
--- main_app/views.py
+++ main_app/views.py
@@ -1,22 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Craft
 
 def home(request):
-  return HttpResponse('<h1>Home</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
 
 def crafts_index(request):
+  crafts = Craft.objects.all()
   return render(request, 'crafts/index.html', { 'crafts': crafts })
-
-class Craft: 
-  def __init__(self, name, type, hours, description):
-    self.name = name
-    self.type = type
-    self.hours = hours
-    self.description = description
-
-crafts = [
-  Craft('Knit Hat', 'knit', '2', 'A warm and cozy hat, made with chunky yarn and love.'),
-]
