a14d60156f1b3c1acb5cba8db69aa05fc37c06d1
Stavon Ewart <stavon@Stavons-MacBook-Air.local>
2020-10-13 13:35:06 -0400

    adds user to craft model and displays on index/detail

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.urls import reverse
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 
@@ -9,6 +9,7 @@ class Craft(models.Model):
   type = models.CharField(max_length=100)
   hours = models.IntegerField()
   description = models.CharField(max_length=300)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
 
   def __str__(self):
     return self.name
