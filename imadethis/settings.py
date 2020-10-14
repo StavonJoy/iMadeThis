e06abba1ef08cb1d8d016bc1896ed55813ad48f1
Stavon Ewart <stavon@Stavons-MacBook-Air.local>
2020-10-14 17:01:56 -0400

    config deployment

--- imadethis/settings.py
+++ imadethis/settings.py
@@ -133,3 +133,8 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/crafts/'
 
 LOGOUT_REDIRECT_URL = '/'
+
+
+# Configure Django App for Heroku.
+import django_on_heroku
+django_on_heroku.settings(locals())
